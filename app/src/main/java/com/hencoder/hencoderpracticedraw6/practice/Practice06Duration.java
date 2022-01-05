d8cebf05d81b8ab0fb3d4ae4dc8484ce001b9906
luyao <luyao@douyu.tv>
2022-01-05 20:25:10 +0800

    【自定义View1-6】作业6提交

--- app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice06Duration.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice06Duration.java
@@ -19,6 +19,7 @@ public class Practice06Duration extends LinearLayout {
     ImageView imageView;
 
     int duration = 300;
+    boolean animated;
 
     public Practice06Duration(Context context) {
         super(context);
@@ -65,6 +66,12 @@ public class Practice06Duration extends LinearLayout {
             @Override
             public void onClick(View v) {
                 // TODO 在这里处理点击事件，执行动画。记得使用 `setDuration(duration)` 来设置动画的时长。
+                if (animated) {
+                    imageView.animate().translationX(getWidth()/2).setDuration(duration);
+                } else {
+                    imageView.animate().translationX(0).setDuration(duration);;
+                }
+                animated = !animated;
             }
         });
     }
